daf53d1899e066953119be8c16e6b7a1218e713b
Roman Decker <roman.decker@gmail.com>
2021-07-11 18:46:29 +0200

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Success } from "./Success";
+
+const password = new Array(10)
+  .fill(null)
+  .reduce(
+    atob,
+    "Vm0wd2VHUXhUWGROVldoVFlteEtXRmxVU205V1JsbDNXa1JTVjJKSGVEQlpNM0JIVmpGYWRHVkliRmROYWxaeVZtcEJlRmRIVmtWUmJVWlhWakpvVFZac1ZtRldNVnBXVFZWV2FHVnFRVGs9"
+  );
 
 function App() {
+  const [formState, setFormState] = React.useState<
+    "pristine" | "validating" | "invalid" | "valid"
+  >("pristine");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {formState === "valid" ? (
+        <Success />
+      ) : (
+        <div className="loginForm">
+          <h1>Login</h1>
+          <form
+            className={formState}
+            onSubmit={(e) => {
+              setFormState("validating");
+              const attempt = e.currentTarget.password.value;
+              setTimeout(() => {
+                setFormState(attempt === password ? "valid" : "invalid");
+              }, 500);
+              e.preventDefault();
+            }}
+          >
+            <div className="fieldGroup">
+              <label htmlFor="password">Password:</label>
+              <input id="password" name="password" type="password" />
+              {formState === "invalid" && (
+                <div className="fieldError">Password invalid. Try again 😛</div>
+              )}
+            </div>
+            <div className="fieldGroup submit">
+              <input
+                type="submit"
+                value={formState === "validating" ? "Logging in..." : "Log in"}
+                disabled={formState === "validating"}
+              />
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
